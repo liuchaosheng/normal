e05f9a61036a5d6ed99c7c612d99fd1aef13c740
zhaoyang <chaoshengl@yeah.net>
2020-04-04 21:00:25 +0800

    修改mainDemo类，用户1修改

--- mydemo/MainDemo.java
+++ mydemo/MainDemo.java
@@ -4,13 +4,15 @@ copyright
 
 package club.zylearn.maindemo;
 
-import club.zylearn.util.ConsoleLog;
-import club.zylearn.util.ConsoleStream;
 import club.zylearn.controller.Controller;
 
 public class MainDemo{
 	public static void main(String args[]){
 		Controller con = new Controller();
 		con.index();
+
+		// 我正在测试中....
+		Object obj = null;
+		System.out.println("hello World!!!!");
 	}
 }
